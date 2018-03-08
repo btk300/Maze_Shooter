cbf4d04c7abc27ec2fc261c8ef1b091b541bdbcd
btk300 <36508646+btk300@users.noreply.github.com>
2018-03-08 11:05:12 +0000

    Added The Gun and a simple script

---
+++ Assets/gun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+
+	public float damage = 15f;
+	public float distance = 150f;
+
+	public Camera fpscam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			RaycastHit hit;
+			if (Physics.Raycast (fpscam.transform.position, fpscam.transform.forward, out hit, distance)) {
+				Debug.Log (hit.transform.name);
+			}
+		}
+	}
+}
