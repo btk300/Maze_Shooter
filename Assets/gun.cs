c6508d6c766fe847fa8059236063501462c84dd6
btk300 <36508646+btk300@users.noreply.github.com>
2018-04-11 20:20:54 +0100

    Revert "Revert "something""
    This reverts commit a2ac62430c5c795adbf72b8856d6ca667ae4be76.

--- Assets/gun.cs
+++ Assets/gun.cs
@@ -7,19 +7,26 @@ public class gun : MonoBehaviour {
 
 	public float damage = 15f;
 	public float distance = 150f;
-
+	public float pushpower;
 	public Camera fpscam;
+	public LayerMask player;
+
+	private Animation gunFire;
 	// Use this for initialization
 	void Start () {
-		
+		gunFire = GetComponent<Animation>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
 			RaycastHit hit;
-			if (Physics.Raycast (fpscam.transform.position, fpscam.transform.forward, out hit, distance)) {
+			if (Physics.Raycast (fpscam.transform.position, fpscam.transform.forward, out hit, distance, player)) {
+				gunFire.Play ();
 				Debug.Log (hit.transform.name);
+				if (hit.rigidbody != null) {
+					hit.rigidbody.AddForce (fpscam.transform.forward * pushpower);
+				}
 			}
 		}
 	}
